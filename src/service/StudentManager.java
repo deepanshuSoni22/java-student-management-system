318b48b1c8fcbbe24cdba6d53f3f235bb7ec61aa
Deepanshu <deepanshusoni022@gmail.com>
2026-04-28 06:08:33 +0000

    feat: add saveToFile method

--- src/service/StudentManager.java
+++ src/service/StudentManager.java
@@ -2,6 +2,8 @@ package service;
 
 import model.Student;
 
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.Collections;
 import java.util.HashMap;
 import java.util.Map;
@@ -9,9 +11,26 @@ import java.util.Map;
 public class StudentManager {
     private static final Map<Integer, Student> map = new HashMap<>();
 
+    private static final String FILE_NAME = "tasks.txt";
+
+    private void saveToFile() {
+        try (FileWriter fw = new FileWriter(FILE_NAME)) {
+            for (Map.Entry<Integer, Student> entry : map.entrySet()) {
+                Integer id = entry.getKey();
+                String student = entry.getValue().getName();
+                // write id and name separated by comma; change fields as needed
+                fw.write(id + ", " + student + System.lineSeparator());
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing to file!");
+        }
+    }
+
+
     public boolean addStudent(Student s) {
         if (!map.containsKey(s.getId())) {
             map.put(s.getId(), s);
+            saveToFile();
             return true;
         }
 
@@ -21,6 +40,7 @@ public class StudentManager {
     public boolean removeStudent(int id) {
         if (map.containsKey(id)) {
             map.remove(id);
+            saveToFile();
             return true;
         }
         return  false;
@@ -29,6 +49,7 @@ public class StudentManager {
     public boolean updateStudent(int id, String name) {
         if (map.containsKey(id)) {
             map.get(id).setName(name);
+            saveToFile();
             return true;
         }
 
